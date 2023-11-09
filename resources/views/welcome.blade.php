ba1a6b74c1a9564ca72c493c087c66796f2ec265
koiwa510 <koiwa@htech-lab.co.jp>
2023-11-09 14:12:07 +0900

    Tailwindのスタイルを当てたボタン追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,12 @@
 
     <p class="title text-2xl font-bold text-blue-500">Blade</p>
 
+    <br>
+
+    <button>Button1</button>
+    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Button2</button>
+    <button class="btn-custom">Button3</button>
+
     <script src="{{ mix('resources/js/app.js') }}" type="module"></script>
 </body>
 
